6ede0fc2990cfbd17e20969ff70ba1afbfa685ea
Kaveri Saibanna <kaveri.saibanna@slksoft.com>
2018-10-11 15:20:23 +0530

    2 commit

---
+++ Restart/Controllers/NewPartialController.cs
@@ -0,0 +1,58 @@
+﻿using Restart.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restart.Controllers
+{
+    public class NewPartialController : Controller
+    {
+        // GET: NewPartial
+        UsersEntities db = new UsersEntities();
+        
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form(tbl_Users u)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.tbl_Users.Add(u);
+                //db.SaveChanges();
+                return RedirectToAction("Display");
+
+            }
+            return PartialView();
+        }
+        public ActionResult Form_Save(tbl_Users u)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                db.tbl_Users.Add(u);
+                db.SaveChanges();
+                return RedirectToAction("Display",u);
+
+            }
+            return PartialView();
+        }
+
+        public ActionResult Display(tbl_Users u)
+        {
+            u.SlNo = u.SlNo != null ? u.SlNo : 0;
+            u.UserName = u.UserName != null ? u.UserName : "";
+            u.EmailId = u.EmailId != null ? u.EmailId : "";
+            u.Password = u.Password != null ? u.Password : "";
+            u.ConfirmPassword = u.ConfirmPassword != null ? u.ConfirmPassword : "";
+            return PartialView(u);
+        }
+        public ActionResult Save()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Restart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Restart.Startup))]
+namespace Restart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
